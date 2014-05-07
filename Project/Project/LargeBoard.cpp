9598f1f5c9e627432d3485c5c9e3bee6daa07260
aheacock <aheacock@vt.edu>
2014-05-07 15:26:10 -0400

    Started Play function for large board

--- Project/Project/LargeBoard.cpp
+++ Project/Project/LargeBoard.cpp
@@ -12,28 +12,20 @@ char LargeBoard::checkWinner() const
 {
 	char returned = ' ';
 	if (board[0][0].checkWinner() == board[0][1].checkWinner() && board[0][1].checkWinner() == board[0][2].checkWinner())
-
 		returned = board[0][0].checkWinner();
 	else if (board[1][0].checkWinner() == board[1][1].checkWinner() && board[1][1].checkWinner() == board[1][2].checkWinner())
-
 		returned = board[1][0].checkWinner();
 	else if (board[2][0].checkWinner() == board[2][1].checkWinner() && board[2][1].checkWinner() == board[1][2].checkWinner())
-
 		returned = board[2][0].checkWinner();
 	else if (board[0][0].checkWinner() == board[1][0].checkWinner() && board[1][0].checkWinner() == board[2][0].checkWinner())
-
 		returned = board[0][0].checkWinner();
 	else if (board[0][1].checkWinner() == board[1][1].checkWinner() && board[1][1].checkWinner() == board[2][1].checkWinner())
-
 		returned = board[0][1].checkWinner();
 	else if (board[0][2].checkWinner() == board[1][2].checkWinner() && board[1][2].checkWinner() == board[2][2].checkWinner())
-
 		returned = board[0][2].checkWinner();
 	else if (board[0][0].checkWinner() == board[1][1].checkWinner() && board[1][1].checkWinner() == board[2][2].checkWinner())
-
 		returned = board[0][0].checkWinner();
 	else if (board[0][2].checkWinner() == board[1][1].checkWinner() && board[1][1].checkWinner() == board[2][0].checkWinner())
-
 		returned = board[0][2].checkWinner();
 	return returned;
 }
@@ -49,4 +41,69 @@ bool LargeBoard::checkDraw() const
 		returned = true;
 	}
 	return returned;
+}
+
+bool LargeBoard::playSpot(int board, int spot, char icon)
+{
+	if (icon == 'X' || icon == 'O')
+	{
+		int x, y;
+		getCoordinates(x, y, board);
+
+	}
+}
+
+void LargeBoard::getCoordinates(int& x, int& y, int spot)
+{
+	if (spot == 1)
+	{
+		x = 0;
+		y = 0;
+	}
+	else if (spot == 2)
+	{
+		x = 1;
+		y = 0;
+	}
+	else if (spot == 3)
+	{
+		x = 2;
+		y = 0;
+	}
+	else if (spot == 4)
+	{
+		x = 0;
+		y = 1;
+	}
+	else if (spot == 5)
+	{
+		x = 1;
+		y = 1;
+	}
+	else if (spot == 6)
+	{
+		x = 2;
+		y = 1;
+	}
+	else if (spot == 7)
+	{
+		x = 0;
+		y = 2;
+	}
+	else if (spot == 8)
+	{
+		x = 1;
+		y = 2;
+	}
+	else if (spot == 9)
+	{
+		x = 2;
+		y = 2;
+	}
+	else
+	{
+		x = -1;
+		y = -1;
+	}
+
 }
--- Project/Project/LargeBoard.h
+++ Project/Project/LargeBoard.h
@@ -10,13 +10,14 @@ class LargeBoard
 {
 private:
 	Board board[3][3];
-
+	void getCoordinates(int& x, int& y, int spot);
 
 public:
 	LargeBoard();
 	void printBoard(ostream& out);
 	bool checkDraw() const;
 	char checkWinner() const;
+	bool playSpot(int board, int spot, char icon);
 
 
 };
