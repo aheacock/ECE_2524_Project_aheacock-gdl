7a1971639e329a517fb804336e4e506ea40586e5
aheacock <aheacock@vt.edu>
2014-05-07 16:42:40 -0400

    more changes/revisions

--- Project/Project/main'.cpp
+++ Project/Project/main'.cpp
@@ -4,19 +4,5 @@ using std::cout;
 using std::cin;
 int main()
 {
-	LargeBoard b;
-	b.printBoard(cout);
-	int big = 0;
-	cin >> big;
-	cout << endl;
-	int small = 0;
-	cin >> small;
-	cout << endl;
-	b.playSpot(big, small, 'X');
-	b.playSpot(big, small+1, 'X');
-	b.playSpot(big, small+2, 'X');
-	b.printSmallBoard(big, cout);
-	b.printBoard(cout);
-
-	return 0;
+	
 }
