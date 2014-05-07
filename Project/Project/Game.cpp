7da37050ce77d2cfdee90ba2349cb6aad50e8daf
aheacock <aheacock@vt.edu>
2014-05-07 16:54:36 -0400

    have code

--- Project/Project/Game.cpp
+++ Project/Project/Game.cpp
@@ -6,40 +6,47 @@
 		LargeBoard a;
 
 }
-void Game::playeroneturn(ostream& out, istream& in)
+bool Game::playeroneturn(ostream& out, istream& in)
 {
+	bool returned = false;
 	int input;
 	int board;
 	out << "X's Turn: Choose Square to play for:";
+	a.printBoard(out);
 	in >> board;
 	a.printSmallBoard(board, out);
 	out << "Choose spot";
 	in >> input;
 	a.playSpot(board, input, 'X');
-
+	a.printSmallBoard(board, out);
 			//bool playSpot(int board, int spot, char icon);
 	
 	if ((a.checkWinner())=='X')
 	{
 		out << "player one wins!!";
+		returned = true;
 	}
 	
 
 }
-void Game::playertwoturn(ostream& out, istream& in)
+bool Game::playertwoturn(ostream& out, istream& in)
 {
+	bool returned = false;
 	int input;
 	int board;
 	out << "O's Turn: Choose Square to play for:";
+	a.printBoard(out);
 	in >> board;
 	a.printSmallBoard(board, out);
 	out << "Choose spot";
 	in >> input;
 	a.playSpot(board, input, 'O');
-
+	a.printSmallBoard(board, out);
 	if ((a.checkWinner()) == 'O')
 	{
 		out << "player one wins!!";
+		returned = true;
 	}
+	return returned;
 
 }
