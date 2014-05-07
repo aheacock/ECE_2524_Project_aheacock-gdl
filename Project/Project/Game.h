d37361846c0ecfa1eb6086a026960ecb771bcae7
dirtymop <gdl@vt.edu>
2014-05-07 15:06:29 -0400

    Added Game Class

---
+++ Project/Project/Game.h
@@ -0,0 +1,21 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include<string>
+#include<iostream>
+using std::string;
+using std::ostream;
+class Game
+{
+private:
+
+
+
+public:
+	Game();
+
+
+};
+
+
+#endif
